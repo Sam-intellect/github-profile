295937eb1461e95bb3909f4386c6e1f14a1ae454
Sam-intellect <alukoolakunlesamuel@gmail.com>
2023-02-18 12:17:01 +0100

    Search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BiSearch } from 'react-icons/bi';
 
 function Search() {
   return (
     <>
-      <div class="bg-lightBlue">
+      <div class="flex justify-between px-3 py-3 border-none bg-lightBlue rounded-2xl focus:ring-grey-500 focus:ring-2">
+        <BiSearch class="text-5xl text-blue" />
+        <form action="">
+          <input
+            type="text"
+            name="search"
+            placeholder="Search Github Username..."
+            autoComplete="off"
+            aria-label="Search Github"
+            class="w-80 py-2 font-semibold bg-lightBlue border-none text-lg text-white"
+          />
+        </form>
         <div></div>
       </div>
     </>
