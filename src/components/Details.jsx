e206b3cc7c536d8940c0d78aeb6a40032f9ae272
Sam-intellect <alukoolakunlesamuel@gmail.com>
2023-02-22 00:24:45 +0100

    mobile done

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { GoLocation } from 'react-icons/go';
+import { BsLink45Deg, BsTwitter, BsLinkedin } from 'react-icons/bs';
 
 function Details() {
   return (
     <>
-      <div className="h-1/2 mt-10 flex flex-col justify-between px-3 py-3 border-none bg-lightBlue rounded-2xl ">
-        <div className=" flex gap-x-8 p-6">
-          <div className="h-32 w-32 bg-white rounded-full border-solid border-2 border-sky-500"></div>
+      <div className="mt-8 flex flex-col justify-between px-3 py-3 border-none bg-lightBlue rounded-2xl ">
+        <div className=" flex gap-x-8 py-6 px-6">
+          <div className="h-24 w-24 bg-white rounded-full border-solid border-2 border-sky-500"></div>
           <div className="flex flex-col gap-2">
             <h1 className="text-2xl font-bold">The Octocat</h1>
             <p className="text-blue text-xl font-bold">@octocat</p>
@@ -13,12 +15,12 @@ function Details() {
           </div>
         </div>
         <div className="px-8">
-          <p className="text-xl font-bold tracking-wide leading-loose font-light">
+          <p className="text-xl font-bold tracking-wide leading-loose font-light text-justify">
             Lorem ipsum dolor sit amet, consectetuer adipiscing eclit. Donec
             odio. Quisque volutpat mattis eros.
           </p>
         </div>
-        <div className=" w-full h-30 mt-4 flex justify-between px-8 py-8 gap-2 border-none bg-deepBlue rounded-2xl text-center">
+        <div className=" w-full h-30 mt-8 flex justify-between px-8 py-8 gap-2 border-none bg-deepBlue rounded-2xl text-center">
           <div>
             <h2>Repo</h2>
             <p>8</p>
@@ -32,29 +34,25 @@ function Details() {
             <p>9</p>
           </div>
         </div>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+
+        <div className="mt-8 flex flex-col gap-4 py-2">
+          <div className="flex gap-4">
+            <GoLocation className="text-2xl font-thin" />
+            <h2>San Francisco</h2>
+          </div>
+          <div className="flex gap-4">
+            <BsLink45Deg className="text-2xl font-thin" />
+            <h2>https://github.blog</h2>
+          </div>
+          <div className="flex gap-4">
+            <BsTwitter className="text-2xl font-thin" />
+            <h2>Not Available</h2>
+          </div>
+          <div className="flex gap-4">
+            <BsLinkedin className="text-2xl font-thin" />
+            <h2>@github</h2>
+          </div>
+        </div>
       </div>
     </>
   );
