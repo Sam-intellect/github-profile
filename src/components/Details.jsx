1e6b8c0d979eb8af3ff5cd7a80a9817e46a773bd
Sam-intellect <alukoolakunlesamuel@gmail.com>
2023-02-23 12:30:52 +0100

    working on API

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { GoLocation } from 'react-icons/go';
 import { BsLink45Deg, BsTwitter, BsLinkedin } from 'react-icons/bs';
 
-function Details() {
+function Details(username) {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    async function fetchUserData() {
+      const response = await fetch(`https://api.github.com/users/${username}`);
+      const data = await response.json();
+      setUserData(data);
+    }
+    fetchUserData();
+  }, [username]);
+
+  if (!userData) {
+    return <div>loading...</div>;
+  }
+
+  const { name, avatar_url, bio, public_repos, followers, following } =
+    userData;
+
   return (
     <>
       <div className="mt-8 flex flex-col justify-between px-3 py-3 border-none bg-lightBlue rounded-2xl ">
         <div className=" flex gap-x-8 py-6 px-6">
           <div className="h-24 w-24 bg-white rounded-full border-solid border-2 border-sky-500"></div>
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col">
             <h1 className="text-2xl font-bold">The Octocat</h1>
             <p className="text-blue text-xl font-bold">@octocat</p>
-            <p className="text-white text-xl font-bold">Joined 25 Jan 2011</p>
+            <p className="text-white text-md font-light">Joined 25 Jan 2011</p>
           </div>
         </div>
-        <div className="px-8">
-          <p className="text-xl font-bold tracking-wide leading-loose font-light text-justify">
+        <div className="p-4">
+          <p className="text-xl font-bold tracking-wide font-light text-justify">
             Lorem ipsum dolor sit amet, consectetuer adipiscing eclit. Donec
             odio. Quisque volutpat mattis eros.
           </p>
@@ -23,15 +41,15 @@ function Details() {
         <div className=" w-full h-30 mt-8 flex justify-between px-8 py-8 gap-2 border-none bg-deepBlue rounded-2xl text-center">
           <div>
             <h2>Repo</h2>
-            <p>8</p>
+            <p className="text-xl font-bold">8</p>
           </div>
           <div>
             <h2>Followers</h2>
-            <p>3938</p>
+            <p className="text-xl font-bold">3938</p>
           </div>
           <div>
             <h2>Following</h2>
-            <p>9</p>
+            <p className="text-xl font-bold">9</p>
           </div>
         </div>
 
